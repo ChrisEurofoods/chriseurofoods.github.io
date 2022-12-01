11f391a2cac429770bb74d8292cfc81afb586086
Chris <christian@eurofoods.fi>
2022-12-01 18:56:10 +0200

    moglord

--- docs/_app/immutable/chunks/2-06e1a182.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c4ec3f19.js";export{t as component};
--- docs/_app/immutable/chunks/3-ac137112.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/en/_page.svelte-107636d1.js";export{t as component};
